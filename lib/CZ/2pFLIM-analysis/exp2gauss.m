c7fa23d26264a88e7e6fbeec4d7f1e5a8c6a8007
barinovi <igorb202@gmail.com>
2021-01-12 18:30:10 -0500

    Documented code for fitting procedures

--- lib/CZ/2pFLIM-analysis/exp2gauss.m
+++ lib/CZ/2pFLIM-analysis/exp2gauss.m
@@ -1,4 +1,11 @@
 function y = exp2gauss(beta0, x)
+% ********* beta0 Parameters **********
+% beta0(1): population 1
+% beta0(2): tau1
+% beta0(3): population 2
+% beta0(4): tau2
+% beta0(5): delta peak time
+% beta0(6): gaussian width
 global gui;
 global spc;
 
@@ -41,60 +48,41 @@ else
 end
 
 
-%t = (1:0.05:50)*spc.datainfo.psPerUnit/1000;
+% Time-point to ps conversion: t = x * spc.datainfo.psPerUnit/1000
 
+% ********* First Exponential calculation ************
+% y1 = decaying exponential, follows form Ae^-t
+% affected by following params: pop1, tau1, gaussian width, delta peak time
 y1 = beta0(1)*exp(tau_g^2/2/tau1^2 - (x-tau_d)/tau1);
+
+% y2 = pulse, follows form erfc(-t)
+% affected by following params: tau1, gaussian width, delta peak time
 y2 = erfc((tau_g^2-tau1*(x-tau_d))/(sqrt(2)*tau1*tau_g));
+
+% y = combination of decay and pulse
+% since y2 (pulse) is ~0 near baseline, y1 (decay) only affects amplitude at/after peak
 y=y1.*y2;
 
-%decay1 = beta0(1)*exp(tau_g^2/2/tau1^2 - (t-tau_d)/tau1);
-%pulse1 = erfc((tau_g^2-tau1*(t-tau_d))/(sqrt(2)*tau1*tau_g));
-%conv1 = decay1.*pulse1;
-
-%"Pre" pulse
+% Pre-pulse calculation: calculates single exponential except shifted to
+% account for residual intensity near baseline
+% y1, y2, and ya are same as above except they are shifted to the left by
+% amount 'pulseI'
 y1 = beta0(1)*exp(tau_g^2/2/tau1^2 - (x-tau_d+pulseI)/tau1);
 y2 = erfc((tau_g^2-tau1*(x-tau_d+pulseI))/(sqrt(2)*tau1*tau_g));
-
 ya = y1.*y2+y;
+
+% Final value for first exponential; ya/2 because adding pre-pulse to
+% regular exponential doubles all amplitudes
 ya = ya/2;
 
-%pre_decay1 = beta0(1)*exp(tau_g^2/2/tau1^2 - (t-tau_d+pulseI)/tau1);
-%pre_pulse1 = erfc((tau_g^2-tau1*(t-tau_d+pulseI))/(sqrt(2)*tau1*tau_g));
-%pre_conv1 = pre_decay1.*pre_pulse1;
-%pulse_int_amp1 = pre_conv1(ceil(spc.datainfo.pulseInt));
-%exp1 = (pre_conv1 + conv1) / 2;
 
-%{
-figure('Name', 'Pulse 1');
-hold on;
-plot(t, exp1);
-plot(t, decay1);
-plot(t, pulse1);
-plot(t, conv1);
-yline(beta0(1));
-yline(pulse_int_amp1, 'g');
-set(gca, 'YScale', 'log');
-legend('exp', 'decay', 'pulse', 'conv', 'pop1', 'pre pulse amp');
-
-figure('Name', 'Pre Pulse 1');
-hold on;
-plot(t, exp1);
-plot(t, pre_decay1);
-plot(t, pre_pulse1);
-plot(t, pre_conv1);
-yline(beta0(1));
-yline(pulse_int_amp1, 'g');
-legend('exp', 'decay', 'pulse', 'conv', 'pop1', 'pre pulse amp');
-set(gca, 'YScale', 'log');
-%}
+% ********* First Exponential calculation ************
+% Calculations are the same except that the second exponential relies on
+% pop2 and tau2 instead of pop1 and tau1
 
 y1 = beta0(3)*exp(tau_g^2/2/tau2^2 - (x-tau_d)/tau2);
 y2 = erfc((tau_g^2-tau2*(x-tau_d))/(sqrt(2)*tau2*tau_g));
 y=y1.*y2;
-
-%decay2 = beta0(3)*exp(tau_g^2/2/tau2^2 - (t-tau_d)/tau2);
-%pulse2 = erfc((tau_g^2-tau2*(t-tau_d))/(sqrt(2)*tau2*tau_g));
-%conv2 = decay2.*pulse2;
 
 y1 = beta0(3)*exp(tau_g^2/2/tau2^2 - (x-tau_d+pulseI)/tau2);
 y2 = erfc((tau_g^2-tau2*(x-tau_d+pulseI))/(sqrt(2)*tau2*tau_g));
@@ -102,43 +90,5 @@ y2 = erfc((tau_g^2-tau2*(x-tau_d+pulseI))/(sqrt(2)*tau2*tau_g));
 yb = y1.*y2+y;
 yb = yb/2;
 
-%pre_decay2 = beta0(3)*exp(tau_g^2/2/tau2^2 - (t-tau_d+pulseI)/tau2);
-%pre_pulse2 = erfc((tau_g^2-tau2*(t-tau_d+pulseI))/(sqrt(2)*tau2*tau_g));
-%pre_conv2 = pre_decay2.*pre_pulse2;
-%pulse_int_amp2 = pre_conv2(ceil(spc.datainfo.pulseInt));
-%exp2 = (pre_conv2+conv2)/2;
-
+% Final value for double exponential
 y=ya+yb;
-
-%full_exp = exp1 + exp2;
-
-%{
-figure('Name', 'Pulse 2');
-hold on;
-plot(t, exp2);
-plot(t, decay2);
-plot(t, pulse2);
-plot(t, conv2);
-yline(beta0(3));
-yline(pulse_int_amp2, 'g');
-set(gca, 'YScale', 'log');
-legend('exp', 'decay', 'pulse', 'conv', 'pop2', 'pre pulse amp');
-
-figure('Name', 'Pre Pulse 2');
-hold on;
-plot(t, exp2);
-plot(t, pre_decay2);
-plot(t, pre_pulse2);
-plot(t, pre_conv2);
-yline(beta0(3));
-yline(pulse_int_amp2, 'g');
-legend('exp', 'decay', 'pulse', 'conv', 'pop2', 'pre pulse amp');
-set(gca, 'YScale', 'log');
-
-figure('Name', 'Fit');
-hold on;
-plot(t, full_exp);
-legend('Fit');
-
-disp('showing fits');
-%}
--- lib/CZ/2pFLIM-analysis/expgauss.m
+++ lib/CZ/2pFLIM-analysis/expgauss.m
@@ -1,9 +1,11 @@
-function y = expgauss(beta0, x);
-%beta0(1): peak
-%beta0(2): exp tau
-%beta0(5): center
-%beta0(6): gaussian width
-% 1/2*erfc[(s^2-tau*x)/{sqrt(2)*s*tau}] * exp[s^2/2/tau^2 - x/tau]
+function y = expgauss(beta0, x)
+% ********* beta0 Parameters **********
+% beta0(1): population 1
+% beta0(2): tau1
+% beta0(3): population 2
+% beta0(4): tau2
+% beta0(5): delta peak time
+% beta0(6): gaussian width
 
 global gui;
 global spc;
@@ -40,14 +42,63 @@ end
 % 
 bg = str2double(get(gui.spc.spc_main.beta7, 'string'));
 
+% Time-point to ps conversion: t = x * spc.datainfo.psPerUnit/1000
+
+% ********* Exponential calculation ************
+
+% y1 = decaying exponential, follows form Ae^-t
+% affected by following params: pop1, tau1, gaussian width, delta peak time
 y1 = beta0(1)*exp(tau_g^2/2/tau1^2 - (x-tau_d)/tau1);
+
+% y2 = pulse, follows form erfc(-t)
+% affected by following params: tau1, gaussian width, delta peak time
 y2 = erfc((tau_g^2-tau1*(x-tau_d))/(sqrt(2)*tau1*tau_g));
+
+% y = combination of decay and pulse
+% since y2 (pulse) is ~0 near baseline, y1 (decay) only affects amplitude at/after peak
 y=y1.*y2;
 
-%"Pre" pulse
+%{
+figure('Name', 'y1');
+plot(t, y1);
+
+figure('Name', 'y2');
+plot(t, y2);
+
+figure('Name', 'y');
+plot(t, y);
+%}
+
+% Pre-pulse calculation: calculates single exponential except shifted to
+% account for residual intensity near baseline
+% y1, y2, and y are same as above except they are shifted to the left by
+% amount 'pulseI'
 y1 = beta0(1)*exp(tau_g^2/2/tau1^2 - (x-tau_d+pulseI)/tau1);
+
 y2 = erfc((tau_g^2-tau1*(x-tau_d+pulseI))/(sqrt(2)*tau1*tau_g));
-
-
 y = y1.*y2+y ;
+a=y;
+
+% Final value for exponential; y/2 because adding pre-pulse to
+% regular exponential doubles all amplitudes
 y=y/2 + bg;
+
+%{
+figure('Name', 'y1');
+plot(t, y1);
+
+figure('Name', 'y2');
+plot(t, y2);
+
+figure('Name', 'y');
+plot(t, y1.*y2);
+
+figure('Name', 'Added');
+plot(t, y1.*y2+a);
+
+figure('Name', 'Fit');
+plot(t, (y1.*y2+a)/2);
+
+figure('Name', 'LT');
+plot(t, lt(1:numel(t)));
+%}
