88bfa94909ace31826d81267d6cdf1e4688cfbe9
barinovi <igorb202@gmail.com>
2021-03-13 18:06:24 -0500

    Moved file menu implementations into 'FileMenu'

--- analysis_1_2_IB/versions/analysis_1_2_IB_031221.m
+++ analysis_1_2_IB/versions/analysis_1_2_IB_031221.m
@@ -110,137 +110,7 @@ end
 %
 function menuOpen_Callback(hObject, ~, ~)
 try
-    handles = guidata(hObject);
-    
-    % Get file paths and store them in a cell
-    fileFilter = {'*.mat', 'MATLAB ROI Files (*.mat)'; ...
-                  '*.csv', 'FLIMage ROI Files (*.csv)'};
-
-    [file, path] = uigetfile(fileFilter, 'Multiselect', 'on');
-    if isequal(file, 0) || isequal(path, 0)
-        return;
-    end
-    filepaths = fullfile(path, file);
-    if ~iscell(filepaths)
-        filepaths = { filepaths };
-    end
-
-    % Move to chosen directory
-    cd(path);
-    
-    % Construct a file object based on the file format
-    if all(AveragedFile.follows_format(filepaths))
-        openFile = AveragedFile(filepaths);
-    elseif all(FLIMageFile.follows_format(filepaths))
-        openFile = FLIMageFile(filepaths);
-    elseif all(PreparedFile.follows_format(filepaths))
-        % Prepared files have raw data as well, need user to choose
-        dataChoice = questdlg('This file has raw and prepared ROI data. Which would you like to load?', ...
-                          'Select ROI Data', ...
-                          'Raw', 'Prepared', 'Cancel', ...
-                          'Cancel');
-
-        switch dataChoice
-            case 'Raw'
-                openFile = RawFile(filepaths);
-            case 'Prepared'
-                openFile = PreparedFile(filepaths);
-            otherwise
-                return;
-        end
-    elseif all(RawFile.follows_format(filepaths))           % <-- Raw is checked after prep since prep can be raw
-        openFile = RawFile(filepaths);
-    else
-        warndlg('The selected files are not supported or are not of the same type.');
-        return;
-    end
-    
-    % Get experiment info
-    dnaTypes = openFile.dna_types();
-    allSolutions = openFile.solution_info();
-    
-    % Check if dna types are same for prepared files
-    if openFile.type() == ROIFileType.Prepared
-        uniqueDNA = unique(dnaTypes);
-        if numel(uniqueDNA) > 1
-            % Let user choose to keep files with different DNA types
-            options = cell(size(filepaths));
-            for i = 1:numel(filepaths)
-                [~, filename, ~] = fileparts(filepaths{i});
-                options{i} = [filename, ': ''', dnaTypes{i}, ''''];
-            end
-            promptStr = {'The files selected have different DNA types.', 'Please select the files you want to load.'};
-            [optionIdx, fileWasSelected] = listdlg('Name', 'File Conflict', ...
-                                                   'PromptString', promptStr, ...
-                                                   'ListString', options, ...
-                                                   'ListSize', [300 300]);
-            if ~fileWasSelected
-                return;
-            end
-            
-            openFile = PreparedFile(filepaths(optionIdx));
-            
-            % Use only unique dna types
-            dnaTypes = unique(openFile.dna_types());
-        end
-    end
-    
-    % Combine dna types
-    dnaType = ROIUtils.combine_dna_types(dnaTypes);
-    % Combine solution info
-    solutionInfo = ROIUtils.combine_solution_info(allSolutions);
-    
-    % Validate solution info
-    if openFile.has_exp_info() && ~ROIUtils.has_number_of_baseline_pts(solutionInfo)
-        warndlg('This file has invalid experiment info. Please fix before loading');
-        return;
-    end
-    
-    % Get data values
-    time = openFile.time();
-    lifetime = openFile.lifetime();
-    int = openFile.green();
-    red = openFile.red();
-    
-    % Generate ROI data object
-    switch openFile.type()
-        case ROIFileType.Averaged
-            normLt = openFile.normalized_lifetime();
-            normInt = openFile.normalized_green();
-            normRed = openFile.normalized_red();
-            roiData = AverageTable(time, lifetime, normLt, int, normInt, red, normRed);
-        otherwise
-            roiData = ROITable(time, lifetime, int, red);
-    end
-    
-    % Update program state
-    AppState.set_open_files(handles, openFile);
-    AppState.set_roi_data(handles, roiData);
-    
-    % Update UI
-    GUIUtils.update_win_title(handles);
-    GUIUtils.update_ui_access(handles, openFile.type());
-    handles = GUIUtils.update_toggle_menu(handles, openFile.roi_count());
-    GUIUtils.update_dna_type(handles, dnaType);
-    GUIUtils.update_solution_info(handles, solutionInfo);
-    
-    % Disable info-dependent controls if necessary
-    if ~openFile.has_exp_info()
-        if GUIUtils.time_is_adjusted(handles)
-            GUIUtils.toggle_button(handles.('btnToggleAdjustedTime'));
-        end
-        if GUIUtils.values_are_normalized(handles)
-            GUIUtils.toggle_button(handles.('btnToggleNormVals'));
-            GUIUtils.toggle_menu(handles.('menuToggleNormVals'));
-        end
-        if GUIUtils.menu_is_toggled(handles.('menuShowAnnots'))
-            GUIUtils.toggle_menu(handles.('menuShowAnnots'));
-        end
-    end
-    
-    % Update data table
-    GUIUtils.update_data_table(handles);         % <-- Updated last because of controls modifying disp
-    
+    FileMenu.open(hObject);
 catch err
     AppState.logdlg(err);
 end
@@ -251,66 +121,7 @@ end
 %
 function menuSave_Callback(hObject, ~, ~)
 try
-    % Get program state
-    handles = guidata(hObject);
-    openFile = AppState.get_open_files(handles);
-    saveData = AppState.get_roi_data(handles);
-    dnaType = GUIUtils.get_dna_type(handles);
-    solutions = GUIUtils.get_solution_info(handles);
-    enabledROIs = GUIUtils.get_enabled_rois(handles);
-
-    % Check if we have all data
-    if isempty(dnaType)
-        warndlg('Please enter a DNA Type before saving');
-        return;
-    end
-    if ~ROIUtils.has_number_of_baseline_pts(solutions)
-        warndlg('Please enter at least 2 solutions with different timings before saving');
-        return;
-    end    
-    
-    % Let user choose to not save disabled ROIs
-    if ~all(enabledROIs)
-        choice = questdlg('Some of the ROIs have been disabled. Would you like to save these ROIs?', ...
-                          'Save Disabled ROIs', ...
-                          'Yes', 'No', 'Cancel', ...
-                          'Cancel');
-        switch choice
-            case 'No'
-                % Leave only enabled ROIs
-                enabledLt = ROIUtils.select(saveData.lifetime(), enabledROIs);
-                enabledInt = ROIUtils.select(saveData.green(), enabledROIs);
-                enabledRed = ROIUtils.select(saveData.red(), enabledROIs);
-                saveData = ROITable(saveData.time(), enabledLt, enabledInt, enabledRed);
-            case 'Cancel'
-                return;
-        end
-    end
-
-    % Let the user select the filepath and file type
-    expNames = openFile.experiment_names();
-    defaultFileName = expNames{1};
-    
-    fileFilter = {'*.mat', 'Raw ROI Files (*.mat)'; ...
-                  '*.mat', 'Prepared ROI Files (*.mat)'; ...
-                  '*.mat', 'Averaged ROI Files (*.mat)'};
-    [file, path, typeIdx] = uiputfile(fileFilter, 'Save ROI File', defaultFileName);
-    if isequal(file, 0) || isequal(path, 0) || isequal(typeIdx, 0)
-        return;
-    end
-    savepath = fullfile(path, file);
-    
-    % Save file according to selected type
-    switch typeIdx
-        case 1
-            RawFile.save(savepath, saveData);
-        case 2
-            PreparedFile.save(savepath, saveData, dnaType, solutions);
-        case 3
-            AveragedFile.save(savepath, saveData, ROIUtils.trim_dna_type(dnaType), solutions);
-        otherwise
-            warndlg('Cannot save the file under this type');
-    end
+    FileMenu.save(hObject);
 catch err
     AppState.logdlg(err);
 end
@@ -321,25 +132,7 @@ end
 %
 function menuClose_Callback(hObject, ~, ~)
 try
-    handles = guidata(hObject);
-    
-    % Get confirmation from user
-    choice = questdlg('Are you sure you want to close this file?', ...
-                      'Confirmation', ...
-                      'Yes', 'No', ...
-                      'No');
-    if strcmp(choice, 'Yes')
-        % Update program state
-        AppState.set_open_files(handles, []);
-        AppState.set_roi_data(handles, []);
-        
-        % Update UI
-        GUIUtils.update_data_table(handles);
-        GUIUtils.update_dna_type(handles, []);
-        GUIUtils.update_solution_info(handles, {});
-        GUIUtils.update_ui_access(handles, ROIFileType.None);
-        GUIUtils.update_win_title(handles);
-    end
+    FileMenu.close(hObject);
 catch err
     AppState.logdlg(err);
 end
