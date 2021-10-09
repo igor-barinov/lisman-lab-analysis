efae7a39376aec5c0a355513c130a6275a12387a
barinovi <igorb202@gmail.com>
2021-10-09 18:22:42 -0400

    All figures will close alongside spc program

--- lib/CZ/FLIM-analysis-modified/scripts/spc_closereq.m
+++ lib/CZ/FLIM-analysis-modified/scripts/spc_closereq.m
@@ -3,6 +3,15 @@ global spcs
 global spc
 global gui
 
+allHandles = fieldnames(gui.spc.figure);
+for i = 1:numel(allHandles)
+    h = gui.spc.figure.(allHandles{i});
+    if ishandle(h)
+        close(h);
+    end
+end
+
+
 spcs = [];
 spc = [];
 closereq;
