18a713734c3fdcea33089c7c2a4f8fb40b715158
barinovi <igorb202@gmail.com>
2021-12-07 18:20:58 -0500

    Added initial value for bg

--- lib/CZ/FLIM-analysis-modified/scripts/spc_initGlobalParams.m
+++ lib/CZ/FLIM-analysis-modified/scripts/spc_initGlobalParams.m
@@ -8,14 +8,15 @@ function spc_initGlobalParams()
     spc.stack.image1{1}=zeros(spc.SPCdata.resolution, spc.datainfo.scan_y, spc.datainfo.scan_x);
     spc.size=size(spc.stack.image1{1});
     spc.SPCdata.line_compression = 0;
-    for i = 1:3
-        spc.fit(i).range = [1,spc.SPCdata.resolution];
-        spc.fit(i).lifetime_limit = [1.7, 2.4];
-        spc.fit(i).lutlim = [0, 30];
-        spc.fit(i).t_offset = 1.0;
-        spc.fit(i).beta0 = [1,0,1,0,0,0];
-        spc.fit(i).fixtau = [0,0,0,0,0,0];
-    end
+% Redefined later, seems redundant - igorb
+%     for i = 1:3
+%         spc.fit(i).range = [1,spc.SPCdata.resolution];
+%         spc.fit(i).lifetime_limit = [1.7, 2.4];
+%         spc.fit(i).lutlim = [0, 30];
+%         spc.fit(i).t_offset = 1.0;
+%         spc.fit(i).beta0 = [1,0,1,0,0,0];
+%         spc.fit(i).fixtau = [0,0,0,0,0,0];
+%     end
     spc.fitIsNew = false;
     spc.switches.logscale = 1;
     spc.SPCdata.scan_size_x = spc.datainfo.scan_x;
@@ -35,7 +36,7 @@ function spc_initGlobalParams()
         spc.fit(j).t_offset = 0;
         spc.fit(j).range = [5, 51];
         spc.fit(j).curve = zeros(spc.fit(j).range(2) - spc.fit(j).range(1) + 1, 1);
-        spc.fit(j).beta0 = [1000, 16.42, 1000, 5.56, 7.24,  0.6199];
+        spc.fit(j).beta0 = [1000, 16.42, 1000, 5.56, 7.24,  0.6199, 0];
         spc.fit(j).fixtau = false(1, 6);
         spc.fit(j).lutlim = [10, 30];
         spc.fit(j).lifetime_limit = [1.4, 2.7];
